be73e394064afd5d21d28a9d8c766f2d1e8cdb92
RishitKothari <69600125+RishitKothari@users.noreply.github.com>
2021-12-12 22:15:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,9 @@
-let c=true;
-let f=false;
-
-document.getElementById('Search').value=city;
-document.getElementById('c').addEventListener('click',function(){  
-    document.getElementById('c').style.backgroundColor='#b71c1c';
-    document.getElementById('f').style.backgroundColor='white';
-    unit=$('#c')[0].name;
-    event.preventDefault();
-    c=true;
-    f=false;
-    if(c){
-        Location.fetchLocation(city,'C');
-    }
-    else{
-        Location.fetchLocation(city,'F');
-
-    }
-});
-document.getElementById('f').addEventListener('click',function(){
-    event.preventDefault();
-    document.getElementById('f').style.backgroundColor='#b71c1c';
-    document.getElementById('c').style.backgroundColor='white';
-    unit=$('#f')[0].name;
-    c=false;
-    f=true;
-    if(c){
-        Location.fetchLocation(city,'C');
-    }
-    else{
-        Location.fetchLocation(city,'F');
-    }
-
-})
-    document.getElementById('Submit').addEventListener('click',function(){
-    event.preventDefault();
-    city=document.getElementById('Search').value;
-    $('#Search')[0].value="";
-    if(c){
-        unit='C';
-    }
-    else{
-        unit='F';
-    }
-    Location.fetchLocation(city,unit);
-});
-
-    let Location={
-        apiKey:"6c65d5c02dc08e8e70fa959a377ba5bd",
+let Location={
+        apiKey:"69074b108b02f806eec79d8ba482a832",
         fetchLocation:function(x,u){
             fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + x +"&appid="+ this.apiKey).then(response=>response.json()).then(data=>{
                 lat=data[0].lat;
                 lon=data[0].lon;
                 weather.fetchWeather(lat,lon,u);
             });
-        }
     }
